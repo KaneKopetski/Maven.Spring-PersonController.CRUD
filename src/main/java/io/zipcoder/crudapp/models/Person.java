efd62ca1072cd8078e77ad6e6f237d06c0ab42d8
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-12-06 19:53:12 -0500

    Complete

---
+++ src/main/java/io/zipcoder/crudapp/models/Person.java
@@ -0,0 +1,35 @@
+package io.zipcoder.crudapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer ID;
+    private String FIRST_NAME;
+    private String LAST_NAME;
+
+    public String getFIRST_NAME() {
+        return FIRST_NAME;
+    }
+
+    public void setFIRST_NAME(String FIRST_NAME) {
+        this.FIRST_NAME = FIRST_NAME;
+    }
+
+    public String getLAST_NAME() {
+        return LAST_NAME;
+    }
+
+    public void setLAST_NAME(String LAST_NAME) {
+        this.LAST_NAME = LAST_NAME;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+}
